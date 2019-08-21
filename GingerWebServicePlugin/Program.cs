d287327f687257a6475bd986e85730766c89b0d5
Ambar <ambar_khan@live.com>
2019-08-21 20:32:11 +0530

    Setting Up Request message

---
+++ GingerWebServicePlugin/Program.cs
@@ -0,0 +1,53 @@
+﻿using Amdocs.Ginger.Plugin.Core;
+using GingerWebServicePlugin.Service;
+using System;
+using System.Collections.Generic;
+
+namespace GingerWebServicePlugin
+{
+    class Program
+    {
+        internal static List<RestService> DriverSessions = new List<RestService>();
+        static void Main(string[] args)
+        {
+
+            AppDomain.CurrentDomain.ProcessExit += new EventHandler(CleanUp);
+            Console.Title = "Rest Plugin";
+            Console.WriteLine("Starting Rest Plugin");
+
+            using (GingerNodeStarter gingerNodeStarter = new GingerNodeStarter())
+            {
+                if (args.Length > 0)
+                {
+                    gingerNodeStarter.StartFromConfigFile(args[0]);  // file name 
+                }
+                else
+                {
+                    //gingerNodeStarter.StartNode("Chrome Service 1", new SeleniumChromeService());
+
+                }
+                gingerNodeStarter.Listen();
+            }
+        }
+
+        private static void CleanUp(object sender, EventArgs e)
+        {
+            foreach (RestService RS in DriverSessions)
+            {
+
+                try
+                {
+                    RS.StopSession();
+                }
+
+                catch
+                {
+
+                }
+            }
+
+        }
+
+    }
+    }
+}
---
+++ GingerWebServicePlugin/Service/RestService.cs
@@ -0,0 +1,45 @@
+﻿using Amdocs.Ginger.Plugin.Core;
+using Amdocs.Ginger.Plugin.Core.ActionsLib;
+using Amdocs.Ginger.Plugin.Core.Attributes;
+using Ginger.Plugin.Platform.WebService;
+using Ginger.Plugin.Platform.WebService.Execution;
+using GingerWebServicePlugin.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GingerWebServicePlugin.Service
+{
+    [GingerService("ResetService", "Rest Service")]
+    public class RestService : IServiceSession, IWebServicePlatform
+    {
+        public IPlatformActionHandler PlatformActionHandler { get; set; } = new WebServicePlatformActionHandler();
+
+
+        #region Plugin Configuration
+
+        [ValidValue(new string[] { "Direct", "Manual", "ProxyAutoConfigure", "AutoDetect", "System" })]
+        [ServiceConfiguration("Proxy Type", "Proxy type")]
+        public string Proxy { get; set; }
+
+
+        [MinLength(10)]
+        [ServiceConfiguration("Proxy Url", "Proxy URL or prixy autoconfig url")]
+        public string ProxyUrl { get; set; }
+        public IRestClient RestClient { get; set; }
+
+        #endregion
+
+
+
+        public void StartSession()
+        {
+            RestClient = new RestClient(Proxy,ProxyUrl);
+        }
+
+        public void StopSession()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
